2b9cf3c5e59a9740c348a5774d79b9fca66f54e8
João Pedro <joao.arodovalho@gmail.com>
2026-05-03 23:38:21 -0300

    atulização move acel
    mover acelelometro testando aaaaa

--- Projeto_Integrador_V/Assets/Scenes/Z_PiTestes/LabMove.cs
+++ Projeto_Integrador_V/Assets/Scenes/Z_PiTestes/LabMove.cs
@@ -2,26 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class LabMove : MonoBehaviour
 {
-    public float speed = 5f;
-    public float smooth = 5f;
-
+    [SerializeField] private float velocidade = 5f;
+    [SerializeField] private float suavidade = 5f;
+    [SerializeField] private float deadzone = 0.05f; // zona neutra
+    
     private Vector2 move;
+
+    // opções de debug
+    [SerializeField] TMP_InputField[] mudarVari;
+    [SerializeField] TMP_Text tiltText;
+
+    void Start()
+    {
+        mudarVari[0].text = $"{velocidade}";
+        mudarVari[1].text = $"{suavidade}";
+        mudarVari[2].text = $"{deadzone}";
+    }
 
     void Update()
     {
-        // Captura a inclinação lateral
-        Vector2 tilt = new Vector2(Input.acceleration.x,Input.acceleration.y);
+        Vector2 tilt;
+        // Ajuste para Landscape Left
+        //tilt = new Vector2(-Input.acceleration.y, Input.acceleration.x);
+        tilt = new Vector2(Input.acceleration.x, Input.acceleration.y);
+        tiltText.text = $"Tilt: {tilt}";
+        TesteUI();
 
-        // Suaviza o movimento (evita tremedeira)
-        move.x = Mathf.Lerp(move.x, tilt.x, Time.deltaTime * smooth);
-        move.y = Mathf.Lerp(move.y, tilt.y, Time.deltaTime * smooth);
+        // Deadzone (para parar no neutro)
+        //if (Mathf.Abs(tilt.x) < deadzone) tilt.x = 0;
+        //if (Mathf.Abs(tilt.y) < deadzone) tilt.y = 0;
 
-        // Move o personagem
+        // Suavização correta
+        move.x = Mathf.Lerp(move.x, tilt.x, Time.deltaTime * suavidade);
+
+        // Movimento no plano XZ
         Vector3 movement = new Vector3(move.x, 0, move.y);
-        transform.Translate(movement * speed * Time.deltaTime);
-        Debug.Log("aaaaaa");
+        transform.Translate(movement * velocidade * Time.deltaTime, Space.World);
+        
+    }
+
+    void TesteUI()
+    {
+        float velocidadeNova, suavidadeNova, deadzoneNova;
+        float.TryParse(mudarVari[0].text, out velocidadeNova);
+        float.TryParse(mudarVari[1].text, out suavidadeNova);
+        float.TryParse(mudarVari[1].text, out deadzoneNova);
+
+        velocidade = velocidadeNova;
+        suavidade = suavidadeNova;
+        deadzone = deadzoneNova;
     }
 }
